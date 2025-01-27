dccda514645331d80edaaf1e93e6a0dee9b88f0d
Kisora Thomas <kisora.thomas@gmail.com>
2025-01-27 16:19:01 -0700

    Update Map.vue iframe attributes
    Removed an unnecessary empty string in the 'allowfullscreen' attribute of the iframe element. This change improves the markup by maintaining a cleaner, more concise format without altering functionality.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -7,7 +7,7 @@
   <iframe
     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51060.937346242565!2d-104.47952005051988!3d36.882965438989004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8710f043b7f65153%3A0xa09ff54108aa82d0!2sRaton%2C%20NM%2087740!5e0!3m2!1sen!2sus!4v1738016456668!5m2!1sen!2sus"
     style="height: 100%; width: 100%; border: none; border-radius: 50px;"
-    allowfullscreen="" loading="lazy"
+    allowfullscreen loading="lazy"
     referrerpolicy="no-referrer-when-downgrade">
   </iframe>
 
